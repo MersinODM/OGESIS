2b0d82cc2d5f2d88a79675034c0dd5301c8cabce
Hakan GÜLEN <hgulen33@gmail.com>
2021-12-10 00:01:37 +0300

    Role ve Permission apileri vol3 sadeleştirme

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -24,29 +24,6 @@ class RoleAndPermissionSeeder extends Seeder
             Permission::create(['name' => $permission, 'slug' => $slug]);
         }
 
-        // create permissions
-//        Permission::create(['name' => 'teacher.create.admin', 'slug' => 'Öğretmen Oluşturma(Üst Yönetici)']);
-//        Permission::create(['name' => 'teacher.create.manager', 'slug' => 'Öğretmen Oluşturma(Müdür)']);
-//        Permission::create(['name' => 'teacher.update.admin', 'slug' => 'Öğretmen Güncelleme(Üst Yönetici)']);
-//        Permission::create(['name' => 'teacher.update.manager', 'slug' => 'Öğretmen Güncelleme(Müdür)']);
-//        Permission::create(['name' => 'teacher.delete', 'slug' => 'Öğretmen Silme']);
-//
-//        Permission::create(['name' => 'team.create.admin', 'slug' => 'Takım Oluşturma(Üst Yönetici)']);
-//        Permission::create(['name' => 'team.create.manager', 'slug' => 'Takım Oluşturma(Müdür)']);
-//        Permission::create(['name' => 'team.update.admin', 'slug' => 'Takım Güncelleme(Üst Yönetici)']);
-//        Permission::create(['name' => 'team.update.manager', 'slug' => 'Takım Güncelleme(Müdür)']);
-//        Permission::create(['name' => 'team.delete', 'slug' => 'Takım Silme']);
-//
-//        Permission::create(['name' => 'team.add-member.admin', 'slug' => 'Takıma Üye Ekleme(Üst Yönetici)']);
-//        Permission::create(['name' => 'team.add-member.manager', 'slug' => 'Takıma Üye Ekleme(Müdür)']);
-//        Permission::create(['name' => 'team.remove-member.admin', 'slug' => 'Takımdan Üye Çıkarma(Üst Yönetici)']);
-//        Permission::create(['name' => 'team.remove-member.manager', 'slug' => 'Takımdan Üye Çıkarma(Müdür)']);
-
-
-//        Permission::create(['name' => 'delete articles']);
-//        Permission::create(['name' => 'publish articles']);
-//        Permission::create(['name' => 'unpublish articles']);
-
         Role::create(['name' => 'admin', 'slug' => 'Sistem Yöneticisi'])
             ->givePermissionTo(Permission::all());
 
