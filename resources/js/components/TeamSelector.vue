5e6715e4f388b6641fa73782abd3764a32bd9a12
Hakan GÜLEN <hgulen33@gmail.com>
2022-01-28 17:07:09 +0300

    Takım seçimi ekleme

--- resources/js/components/TeamSelector.vue
+++ resources/js/components/TeamSelector.vue
@@ -1,8 +1,34 @@
-<template />
+<template>
+  <div class="form-group">
+    <label>Takım Seçimi</label>
+    <multiselect
+      v-model="selectedTeam"
+      :name="name"
+      placeholder="Takım seçebilirsiniz"
+      no-options-text="Bu liste boş!"
+      no-result-text="Burada bişey bulamadık!"
+      :close-on-select="true"
+      track-by="name"
+      :min-chars="2"
+      value-prop="id"
+      :searchable="true"
+      label="name"
+      :options="teamList"
+      class="form-control"
+      :class="{'is-invalid': isValidated && errorMessage != null}"
+    />
+    <validation-error
+      v-if="isValidated"
+      v-model="errorMessage"
+    />
+  </div>
+</template>
 
 <script>
 import ValidationError from './ValidationError'
 import Multiselect from '@vueform/multiselect'
+import { useModelWrapper } from '../compositions/useModelWrapper'
+import { useComponentValidationWrapper } from '../compositions/useComponentValidationWrapper'
 
 export default {
   name: 'TeamSelector',
@@ -21,13 +47,20 @@ export default {
       type: Boolean,
       default: false
     },
-    teachers: {
+    teams: {
       type: Array,
       default: () => ([])
     },
     name: {
       type: String,
       default: ''
+    }
+  },
+  setup (props, { emit }) {
+    return {
+      selectedTeam: useModelWrapper(props, emit),
+      teamList: useModelWrapper(props, emit, 'teams'),
+      ...useComponentValidationWrapper(props) // Buradan validasyon parametreleri geliyor
     }
   }
 }
