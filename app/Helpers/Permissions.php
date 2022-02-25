1eb3d6c9e33a497d1b9ba7fe78b4ac145016d313
Hakan GÜLEN <hgulen33@gmail.com>
2022-02-25 15:03:26 +0300

    yetkiler düzenlendi vol 2

--- app/Helpers/Permissions.php
+++ app/Helpers/Permissions.php
@@ -7,6 +7,7 @@ namespace App\Helpers;
  * INSTITUTION ve CREATE yetkisine sahip kullancı için LEVEL_2 veya LEVEL_3 yetki lazımdır
  * Burada tüm kurumlara CRUD ve SUBJE yetkileri verilebilir ancak seviyeler mutlaka kurumun seviyesinde olmalıdır
  */
+
 class Permissions
 {
 
@@ -15,12 +16,12 @@ class Permissions
     public const LEVEL_2 = 'level.2';
     public const LEVEL_1 = 'level.1';
 
-    public const CREATE = 'create';
-    public const DELETE = 'delete';
-    public const UPDATE = 'update';
-    public const LIST = 'list';
-    public const SHOW = 'show';
-    public const UPLOAD = 'upload';
+    private const CREATE = 'create';
+    private const DELETE = 'delete';
+    private const UPDATE = 'update';
+    private const LIST = 'list';
+    private const SHOW = 'show';
+    private const UPLOAD = 'upload';
 
     public const TEACHER = 'teacher';
     public const BRANCH = 'branch';
@@ -34,7 +35,61 @@ class Permissions
     public const PARTNER = 'partner';
     public const PERSONAL = 'personal';
 
+    public const TEACHER_CREATE = self::TEACHER . '.' . self::CREATE;
+    public const TEACHER_UPDATE = self::TEACHER . '.' . self::UPDATE;
+    public const TEACHER_DELETE = self::TEACHER . '.' . self::DELETE;
+    public const TEACHER_LIST = self::TEACHER . '.' . self::LIST;
+    public const TEACHER_SHOW = self::TEACHER . '.' . self::SHOW;
+    public const TEACHER_UPLOAD = self::TEACHER . '.' . self::UPLOAD;
 
+    public const BRANCH_CREATE = self::BRANCH . '.' . self::CREATE;
+    public const BRANCH_UPDATE = self::BRANCH . '.' . self::UPDATE;
+    public const BRANCH_DELETE = self::BRANCH . '.' . self::DELETE;
+    public const BRANCH_LIST = self::BRANCH . '.' . self::LIST;
+
+    public const TEAM_CREATE = self::TEAM . '.' . self::CREATE;
+    public const TEAM_UPDATE = self::TEAM . '.' . self::UPDATE;
+    public const TEAM_DELETE = self::TEAM . '.' . self::DELETE;
+    public const TEAM_LIST = self::TEAM . '.' . self::LIST;
+
+    public const INSTITUTION_CREATE = self::INSTITUTION . '.' . self::CREATE;
+    public const INSTITUTION_UPDATE = self::INSTITUTION . '.' . self::UPDATE;
+    public const INSTITUTION_DELETE = self::INSTITUTION . '.' . self::DELETE;
+    public const INSTITUTION_LIST = self::INSTITUTION . '.' . self::LIST;
+    public const INSTITUTION_UPLOAD = self::INSTITUTION . '.' . self::UPLOAD;
+
+    public const PLAN_CREATE = self::PLAN . '.' . self::CREATE;
+    public const PLAN_UPDATE = self::PLAN . '.' . self::UPDATE;
+    public const PLAN_DELETE = self::PLAN . '.' . self::DELETE;
+    public const PLAN_LIST = self::PLAN . '.' . self::LIST;
+
+    public const ACTIVITY_CREATE = self::ACTIVITY . '.' . self::CREATE;
+    public const ACTIVITY_UPDATE = self::ACTIVITY . '.' . self::UPDATE;
+    public const ACTIVITY_DELETE = self::ACTIVITY . '.' . self::DELETE;
+    public const ACTIVITY_LIST = self::ACTIVITY . '.' . self::LIST;
+
+    public const REPORT_CREATE = self::REPORT . '.' . self::CREATE;
+    public const REPORT_UPDATE = self::REPORT . '.' . self::UPDATE;
+    public const REPORT_DELETE = self::REPORT . '.' . self::DELETE;
+    public const REPORT_LIST = self::REPORT . '.' . self::LIST;
+
+    public const THEME_CREATE = self::THEME . '.' . self::CREATE;
+    public const THEME_UPDATE = self::THEME . '.' . self::UPDATE;
+    public const THEME_DELETE = self::THEME . '.' . self::DELETE;
+    public const THEME_LIST = self::THEME . '.' . self::LIST;
+
+    public const DISTRICT_CREATE = self::DISTRICT . '.' . self::CREATE;
+    public const DISTRICT_UPDATE = self::DISTRICT . '.' . self::UPDATE;
+    public const DISTRICT_DELETE = self::DISTRICT . '.' . self::DELETE;  // Burada soft delete olabilir
+    public const DISTRICT_LIST = self::DISTRICT . '.' . self::LIST;
+
+    public const PARTNER_CREATE = self::PARTNER . '.' . self::CREATE;
+    public const PARTNER_UPDATE = self::PARTNER . '.' . self::UPDATE;
+    public const PARTNER_DELETE = self::PARTNER . '.' . self::DELETE;
+    public const PARTNER_LIST = self::PARTNER . '.' . self::LIST;
+
+    public const PERSONAL_PASSWORD_CHANGE = self::PERSONAL . '.password_change';
+    public const PERSONAL_UPDATE = self::PERSONAL . '.' . self::UPDATE;
 
 
     public static function getPermissions(): array
@@ -47,26 +102,51 @@ class Permissions
             self::LEVEL_2 => 'İlçe Müdürlüğü Yetkisi',
             self::LEVEL_1 => 'Okul Müdürlüğü Yetkisi',
 
-            //Burada aksiyon yetkileri
-            self::CREATE => 'Oluşturma Yetkisi',
-            self::UPDATE => 'Güncelleme Yetkisi',
-            self::DELETE => 'Silme Yetkisi',
-            self::LIST => 'Listeleme Yetkisi',
-            self::SHOW => 'Gösterme Yetkisi',
-            self::UPLOAD => 'Yükleme Yetkisi',
-
-            //Burada subje yetkileri
-            self::TEACHER => 'Öğretmen İşlemleri Yetkisi',
-            self::TEAM => 'Takım İşlemleri Yetkisi',
-            self::THEME => 'Tema İşlemleri Yetkisi',
-            self::INSTITUTION => 'Kurum İşlemleri Yetkisi',
-            self::PLAN => 'Gelişim Planı İşlemleri Yetkisi',
-            self::ACTIVITY => 'Etkinlik İşlemleri Yetkisi',
-            self::REPORT => 'Rapor İşlemleri Yetkisi',
-            self::PERSONAL => 'Kişisel İşlemler Yetkisi',
-            self::BRANCH => 'Alan/Branş İşlemleri Yetkisi',
-            self::DISTRICT => 'İlçe İşlemleri Yetkisi',
-            self::PARTNER => 'Partner/Paydaş İşlemleri Yetkisi',
+            self::TEACHER_CREATE => 'Öğretmen oluşturma yetkisi',
+            self::TEACHER_UPDATE => 'Öğretmen güncelleme yetkisi',
+            self::TEACHER_DELETE => 'öğretmen silme yetkisi', // Bu yetki dikkatli kullanılmalı
+            self::TEACHER_LIST => 'Öğretmen listeleme yetkisi',
+            self::TEACHER_SHOW => 'Öğretmen gösterme yetkisi',
+            self::TEACHER_UPLOAD => 'Öğretmenleri toplu yükleme yetkisi',
+            self::BRANCH_CREATE => 'Alan/Branş oluşturma yetkisi',
+            self::BRANCH_UPDATE => 'Alan/Branş güncelleme yetkisi',
+            self::BRANCH_DELETE => 'Alan/Branş silme yetkisi',
+            self::BRANCH_LIST => 'Alan/Branş listeleme yetkisi',
+            self::TEAM_CREATE => 'Takım/Ekip oluşturma yetkisi',
+            self::TEAM_UPDATE => 'Takım/Ekip güncelleme yetkisi',
+            self::TEAM_DELETE => 'Takım/Ekip silme yetkisi',
+            self::TEAM_LIST => 'Takım listeleme yetkisi',
+            self::INSTITUTION_CREATE => 'Kurum listeleme yetkisi',
+            self::INSTITUTION_UPDATE => 'Kurum güncelleme yetkisi',
+            self::INSTITUTION_DELETE => 'Kurum silme yetkisi', // Kurum silme softdelete şeklinde düşünülmeli vey aktif pasif yapılmalı aynı şey gerçi
+            self::INSTITUTION_LIST => 'Kurum listeleme yetkisi',
+            self::INSTITUTION_UPLOAD => 'Kurumları toplu yükleme yetkisi',
+            self::PLAN_CREATE => 'Plan oluşturma yetkisi',
+            self::PLAN_UPDATE => 'Plan güncelleme yetkisi',
+            self::PLAN_DELETE => 'Plan silme yetkisi', // Plan girişi yapılmışsa aktarma yapılmalı yeni plana
+            self::PLAN_LIST => 'Plan listeleme yetkisi',
+            self::ACTIVITY_CREATE => 'Aktivite/Etkinlik oluşturma yetkisi',
+            self::ACTIVITY_UPDATE => 'Aktivite/Etkinlik güncelleme yetkisi',
+            self::ACTIVITY_DELETE => 'Aktivite/Etkinlik silme yetkisi',
+            self::ACTIVITY_LIST => 'Aktivite/Etkinlik listeleme yetkisi',
+            self::REPORT_CREATE => 'Rapor oluşturma yetkisi',
+            self::REPORT_UPDATE => 'Rapor güncelleme yetkisi',
+            self::REPORT_DELETE => 'Rapor silme yetkisi',
+            self::REPORT_LIST => 'Rapor listeleme yetkisi',
+            self::THEME_CREATE => 'Tema oluşturma yetkisi',
+            self::THEME_UPDATE => 'Tema güncelleme yetkisi',
+            self::THEME_DELETE => 'Tema silme yetkisi', // Soft delete ya aktif pasif etme durumu söz konusu olabilir
+            self::THEME_LIST => 'Tema listeleme yetkisi',
+            self::DISTRICT_CREATE => 'İlçe oluşturma yetkisi',
+            self::DISTRICT_UPDATE => 'İlçe güncelleme yetkisi',
+            self::DISTRICT_DELETE => 'İlçe silme yetkisi',
+            self::DISTRICT_LIST => 'İlçe listeleme yetkisi',
+            self::PARTNER_CREATE => 'Paydaş/Partner oluşturma yetkisi',
+            self::PARTNER_UPDATE => 'Paydaş/Partner güncelleme yetkisi',
+            self::PARTNER_DELETE => 'Paydaş/Partner silme yetkisi',
+            self::PARTNER_LIST => 'Paydaş/Partner listeleme yetkisi',
+            self::PERSONAL_PASSWORD_CHANGE => 'Kişisel şifre değiştirme yetkisi',
+            self::PERSONAL_UPDATE => 'Kişisel bilgileri güncelleme yetkisi',
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -34,72 +34,72 @@ Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1'], static function
 
     // İlçeler api endpoint tanımları
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::DISTRICT]], static function () {
-        Route::get('districts', [DistrictController::class, 'list'])->can(Permissions::LIST);
+        Route::get('districts', [DistrictController::class, 'list'])->can(Permissions::DISTRICT_LIST);
     });
 
     // Planlama api endpoint ve yetki tanımları
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::PLAN]], static function () {
-        Route::post('plans', [PlanController::class, 'createAll'])->can(Permissions::CREATE);
-        Route::put('plans/{id}', [PlanController::class, 'update'])->can(Permissions::UPDATE);
-        Route::get('plans/latest/{count}', [PlanController::class, 'getLastPlans'])->can(Permissions::LIST);
+        Route::post('plans', [PlanController::class, 'createAll'])->can(Permissions::PLAN_CREATE);
+        Route::put('plans/{id}', [PlanController::class, 'update'])->can(Permissions::PLAN_UPDATE);
+        Route::get('plans/latest/{count}', [PlanController::class, 'getLastPlans'])->can(Permissions::PLAN_LIST);
     });
 
     // Kurum endpoint ve yetki tanımlamaları
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::INSTITUTION]], static function () {
-        Route::post('institutions', [InstitutionController::class, 'create'])->can(Permissions::CREATE);
-        Route::post('institutions/import', [InstitutionController::class, 'importFromExcel'])->can(Permissions::CREATE);
-        Route::post('institutions/table', [InstitutionController::class, 'getTable'])->can(Permissions::LIST);
-        Route::put('institutions/{id}', [InstitutionController::class, 'update'])->can(Permissions::UPDATE);
-        Route::get('institutions/search_by', [InstitutionController::class, 'searchBy'])->can(Permissions::LIST);
-        Route::get('districts/{id}/institutions', [InstitutionController::class, 'get'])->can(Permissions::LIST);
+        Route::post('institutions', [InstitutionController::class, 'create'])->can(Permissions::INSTITUTION_CREATE);
+        Route::post('institutions/import', [InstitutionController::class, 'importFromExcel'])->can(Permissions::INSTITUTION_UPLOAD);
+        Route::post('institutions/table', [InstitutionController::class, 'getTable'])->can(Permissions::INSTITUTION_LIST);
+        Route::put('institutions/{id}', [InstitutionController::class, 'update'])->can(Permissions::INSTITUTION_UPDATE);
+        Route::get('institutions/search_by', [InstitutionController::class, 'searchBy'])->can(Permissions::INSTITUTION_LIST);
+        Route::get('districts/{id}/institutions', [InstitutionController::class, 'get'])->can(Permissions::INSTITUTION_LIST);
     });
 
     // Tema endpoint ve yetki tanımlamaları
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::THEME]], static function () {
-        Route::post('themes', [ThemeController::class, 'create'])->can(Permissions::CREATE);
-        Route::put('themes/{id}', [ThemeController::class, 'update'])->can(Permissions::UPDATE);
-        Route::get('themes', [ThemeController::class, 'getThemes'])->can(Permissions::LIST);
-        Route::get('districts/{district_id}/institutions/{institution_id}/teams', [TeamController::class, 'get'])->can(Permissions::LIST);
+        Route::post('themes', [ThemeController::class, 'create'])->can(Permissions::THEME_CREATE);
+        Route::put('themes/{id}', [ThemeController::class, 'update'])->can(Permissions::THEME_UPDATE);
+        Route::get('themes', [ThemeController::class, 'getThemes'])->can(Permissions::THEME_LIST);
+        Route::get('districts/{district_id}/institutions/{institution_id}/teams', [TeamController::class, 'get'])->can(Permissions::THEME_LIST);
     });
 
     // Aktivite endpoint ve yetki denetimleri
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::ACTIVITY]], static function () {
-        Route::post('activities', [ActivityController::class, 'create'])->can(Permissions::CREATE);
-        Route::put('activities/{id}', [ActivityController::class, 'update'])->can(Permissions::UPDATE);
-        Route::get('activities', [ActivityController::class, 'list'])->can(Permissions::LIST);
-    });
+        Route::post('activities', [ActivityController::class, 'create'])->can(Permissions::ACTIVITY_CREATE);
+        Route::put('activities/{id}', [ActivityController::class, 'update'])->can(Permissions::ACTIVITY_UPDATE);
+        Route::get('activities', [ActivityController::class, 'list'])->can(Permissions::ACTIVITY_LIST);
+    });REPORT
 
     // Öğretmen endpoint ve yetki tanımlamaları
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::TEACHER]], static function () {
-        Route::post('teachers', [TeacherController::class, 'create'])->can(Permissions::CREATE);
-        Route::put('teachers/{id}', [TeacherController::class, 'update'])->can(Permissions::UPDATE);
-        Route::post('teachers/table', [TeacherController::class, 'getTable'])->can(Permissions::LIST);
-        Route::get('districts/{district_id}/institutions/{institution_id}/teachers', [TeacherController::class, 'get'])->can(Permissions::LIST);
+        Route::post('teachers', [TeacherController::class, 'create'])->can(Permissions::TEACHER_CREATE);
+        Route::put('teachers/{id}', [TeacherController::class, 'update'])->can(Permissions::TEACHER_UPDATE);
+        Route::post('teachers/table', [TeacherController::class, 'getTable'])->can(Permissions::TEACHER_LIST);
+        Route::get('districts/{district_id}/institutions/{institution_id}/teachers', [TeacherController::class, 'get'])->can(Permissions::TEACHER_LIST);
     });
 
     // Takım endpoint ve yetki tanımlamaları
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::TEAM]], static function () {
-        Route::post('teams', [TeamController::class, 'create'])->can(Permissions::CREATE);
-        Route::put('teams/{id}', [TeamController::class, 'update'])->can(Permissions::UPDATE);
-        Route::post('teams/table', [TeamController::class, 'getTable'])->can(Permissions::LIST);
+        Route::post('teams', [TeamController::class, 'create'])->can(Permissions::TEAM_CREATE);
+        Route::put('teams/{id}', [TeamController::class, 'update'])->can(Permissions::TEAM_UPDATE);
+        Route::post('teams/table', [TeamController::class, 'getTable'])->can(Permissions::TEAM_LIST);
     });
 
     //Branş endpoint ve yetki tanımlamaları
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::BRANCH]], static function (){
-        Route::post('branches', [BranchController::class, 'create'])->can(Permissions::CREATE);
-        Route::put('branches/{id}', [BranchController::class, 'update'])->can(Permissions::UPDATE);
-        Route::get('branches/search_by', [BranchController::class, 'searchBy'])->can(Permissions::LIST);
+        Route::post('branches', [BranchController::class, 'create'])->can(Permissions::BRANCH_CREATE);
+        Route::put('branches/{id}', [BranchController::class, 'update'])->can(Permissions::BRANCH_UPDATE);
+        Route::get('branches/search_by', [BranchController::class, 'searchBy'])->can(Permissions::BRANCH_LIST);
     });
 
     //Partner endpoint ve yetki tanılamaları
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::PARTNER]], static function () {
-        Route::get('partners/search_by', [PartnerController::class, 'searchBy'])->can(Permissions::LIST);
+        Route::get('partners/search_by', [PartnerController::class, 'searchBy'])->can(Permissions::PARTNER_LIST);
     });
 
     //Report endpoint ve yetki tanımlamaları
     Route::group(['middleware' => ['role_or_permission:super-admin|'. Permissions::REPORT]], static function () {
-        Route::post('report-requests', [ReportRequestController::class, 'create'])->can(Permissions::CREATE);
-        Route::post('report-requests/table', [ReportRequestController::class, 'getTable'])->can(Permissions::LIST);
+        Route::post('report-requests', [ReportRequestController::class, 'create'])->can(Permissions::REPORT_CREATE);
+        Route::post('report-requests/table', [ReportRequestController::class, 'getTable'])->can(Permissions::REPORT_LIST);
         // TODO rapor yükleme tamamlanacak
         Route::post('institutions/{institutionId}/reports/upload');
     });
